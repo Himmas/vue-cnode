c512d24f889b2a00a46cef68c19a5d2db8c259c0
kevinlorry <1197318313@qq.com>
2017-02-22 21:02:58 +0800

    no message

--- src/components/commonpage/headComp.vue
+++ src/components/commonpage/headComp.vue
@@ -1,11 +1,22 @@
 <template>
-    <div>
-        <router-link to="/">Home</router-link>
+    <div >
+        <div id="top">
+            <div id="logo">
+                <router-link to="/"><img src="../../assets/cnodejs_light.svg"/></router-link>
+            </div>
+            
+        </div>
         <router-link to="/login">login</router-link>
     </div>
 </template>
 <style lang="less">
-
+   
+    #logo {
+        img {
+         width: 50%;
+         height：10%;
+        } 
+    }
 </style>
 <script>
 
