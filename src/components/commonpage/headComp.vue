1a4745bb8e69b23d91cc507d35228c922973c59e
superBayMax <lxrcshik@sina.com>
2017-02-24 10:02:38 +0800

    首页改好了；冲突也解决了

--- src/components/commonpage/headComp.vue
+++ src/components/commonpage/headComp.vue
@@ -1,95 +1,68 @@
 <template>
-<<<<<<< HEAD
-    <div >
-      <div class="top">
-        <div class="logo">
-          <router-link to="/"><img src="../../assets/cnodejs_light.svg"/></router-link>
+
+    <div>
+        <div class="top">
+            <div class="logo">
+                <img src="../../assets/cnodejs_light.svg"/>
+            </div>
+            <div class="search">
+                <form method="get" action="http://www.baidu.com/s">
+                    <input type="text" class="search-input" name="wd" autocomplete="off"/>
+                </form>
+            </div>
+            <div class="leder">
+                <ul>
+                    <li v-for="item in items" class="leder-li">
+                       <router-link :to='item.path' class="leder-a" tag="a">{{item.name}}</router-link>
+                     </li>
+                </ul>
+            </div>
         </div>
-        <div class="search">
-          <form  method="get" action="http://www.baidu.com/s">
-            <input type="text" class="search-input" name="wd" autocomplete="off"/>
-          </form>
-        </div>
-        <div class="leder">
-          <ul>
-            <li v-for="item in items" class="leder-li" >
-            <a href="" class="leder-a">{{item}}</a>
-            </li>
-          </ul>
-        </div>
-      </div>
-      <router-link to="/login">login</router-link>
     </div>
 </template>
 <style lang="less">
     .logo {
-      img {
-        width:30%;
-      }
-    text-align:center;
-    }
-    .top {
-      width: 98%;
-      background-color: #444;
-      margin:0 auto;
-    }
-    .search {
-      text-align:center;
-=======
-    <div>
-        <div id="top">
-            <div id="logo">
-                <router-link to="/"><img src="../../assets/cnodejs_light.svg"/></router-link>
-            </div>
-            <div id="search">
-                <form method="get" action="http://www.baidu.com/s">
-                    <input type="text" name="wd" autocomplete="off"/>
-                </form>
-            </div>
-        </div>
-        <router-link to="/login">登陆</router-link>
-    </div>
-</template>
-<style lang="less">
-    #logo {
         img {
             width: 30%;
         }
         text-align: center;
     }
 
-    #top {
+    .top {
         width: 100%;
-        background-color: #555;
+        background-color: #444;
         margin: 0 auto;
     }
 
-    #search {
+    .search {
         text-align: center;
->>>>>>> 35fbe2d5dcf43be66113e05a8664dad6f93b6d16
     }
+
     .search-input {
-      border-radius: 15px;
-      width: 60%;
-      height: 25px;
-      background: url("../../assets/search.png") no-repeat left center;
-      background-size: 20px 20px;
-      background-color: #888;
-      padding-left: 22px;
-      }
-      .search-input:hover{
+        border-radius: 15px;
+        width: 60%;
+        height: 25px;
+        background: url("../../assets/search.png") no-repeat left center;
+        background-size: 20px 20px;
+        background-color: #888;
+        padding-left: 22px;
+    }
+
+    .search-input:hover {
         background-color: white;
     }
-    .leder-a{
-      color: white;
+
+    .leder-a {
+        color: white;
     }
+
     .leder-li:hover {
-      opacity: 0.3;
+        opacity: 0.3;
     }
 
     .leder-li {
-      display: inline-block;
-      padding:20px 3% 20px 3%;
+        display: inline-block;
+        padding: 20px 3% 20px 3%;
 
     }
 </style>
@@ -98,7 +71,19 @@
   export default{
     data(){
       return{
-        items:["首页","新手入门","API","关于","注册","登录"],
+        items:[{
+           path:'/',
+           name:'首页'
+        },{
+            path:'/',
+            name:'新手入门'
+        },{
+            path:'/',
+            name:'Api'
+        },{
+            path:'/login',
+            name:'登录'
+        }]
       }
     },
     created(){
@@ -112,5 +97,16 @@
 
     }
   }
+
+
+
+
+
+
+
+
+
+
+
 
 </script>
