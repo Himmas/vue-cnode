cd72c1994b86d900c921bc3fc00facb3c4f314ee
kevinlorry <1197318313@qq.com>
2017-02-27 00:13:01 +0800

    写好了文章list样式

--- src/components/article/tabBar.vue
+++ src/components/article/tabBar.vue
@@ -1,40 +1,66 @@
 <template>
-  <div>
-    <ul>
-      <li class="list" v-for="list in lists">
-        <router-link :to="list.tab">{{list.name}}</router-link>
-      </li>
-    </ul>
-
+  <div class="main">
+    <div class="main-top">
+      <ul>
+        <li class="list" v-for="(list ,index) in lists" :class="{active:isactive==index}"  @click="actived(index)">
+          <router-link :to="list.tab">{{list.name}}</router-link>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 <style lang="less">
-
+  .main{
+    width: 98%;
+    margin:0 auto;
+  }
+  .main-top{
+     background-color:#f6f6f6;
+   }
+  .active {
+    background-color:#80bd01;
+  }
+  .list{
+    display: inline-block;
+    padding: 10px 5%;
+    font-size: 15px;
+  }
+  .list a{
+    color:#80bd01
+  }
+  .active a{
+    color:white;
+  }
 </style>
 <script>
 
   export default{
     data(){
       return {
+      	isactive:null,
         lists:[{
-          tab:"all",
-          name:"全部"
+          tab:"?tab=all",
+          name:"全部",
           },
           {
-          	tab:"esse",
-            name:"精华"
+          	tab:"?tab=esse",
+            name:"精华",
           },
           {
-            tab:"share",
-            name:"分享"
+            tab:"?tab=share",
+            name:"分享",
           },
           {
-            tab:"aks",
-            name:"问答"
-          },
+            tab:"?tab=ask",
+            name:"问答",
+          }
         ]
       }
     },
-    components: {}
+    methods:{
+      actived(index){
+      	this.isactive =index
+      }
+    }
   }
 </script>
