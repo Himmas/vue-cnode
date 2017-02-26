3364c4737ba5b7972e69c6d0b2a92361d5e535c4
kylin-z <396882574@qq.com>
2017-02-26 15:04:18 +0800

    删除多余组件

--- src/router/routes.js
+++ src/router/routes.js
@@ -6,11 +6,6 @@
 import article from '../components/article/articleComp'
 import login from '../components/login/login.vue'
 //子router-view
-import newArticle from '../components/article/article-type/newArticle.vue'
-import esseArticle from '../components/article/article-type/esseArticle.vue'
-import shaerArticle from '../components/article/article-type/shareArticle.vue'
-import askArticle from '../components/article/article-type/askArticle.vue'
-
 import articleList from '../components/article/articleList.vue'
 
 const routes = [{
