984d6528defd41e6346cbcf1031ace6a0d66426d
superBayMax <lxrcshik@sina.com>
2017-03-01 10:37:34 +0800

    添加详情页

---
+++ src/components/article/articleDetail.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+
+  </div>
+</template>
+<style lang="less">
+
+</style>
+<script>
+
+  export default {
+    data() {
+      return {
+
+      }
+    },
+    created(){
+
+    },
+
+    methods:{
+
+    },
+    components: {
+
+    }
+  }
+</script>
