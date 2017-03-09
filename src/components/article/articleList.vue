9eb15c67738ffbaaeb9551f667e949ff3f3ae5e8
kevinlorry <1197318313@qq.com>
2017-03-09 22:43:13 +0800

    写了一点点。。。

--- src/components/article/articleList.vue
+++ src/components/article/articleList.vue
@@ -1,4 +1,20 @@
 <template>
+    <div class="articlelist">
+        <ul>
+            <li class="art-li" v-for="artlist in artlists">
+                <div class="art-head">
+                    <img src=""/>
+                </div>
+                <div class="art-tab">
+                    {{artlist.tab}}
+
+                </div>
+                <router-link :to="{name = 'topic',params:{id:artlist.id }}">
+
+                </router-link>
+            </li>
+        </ul>
+    </div>
 
 </template>
 <style lang="less" scoped>
